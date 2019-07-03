d1b5a2025fb5055061cc3535a2fb8847c9c8c962
Arthur Marinho dos Passos <arthur.passos@ufma.deinf.local>
2019-07-03 18:46:10 -0300

    tentando fazer o canhão atirar

--- Alg1-Jogo-Lua/7rings/codigo/diretorio/jogo.lua
+++ Alg1-Jogo-Lua/7rings/codigo/diretorio/jogo.lua
@@ -2,12 +2,14 @@ require 'codigo/diretorio/heroina'
 require 'codigo/diretorio/castelo'
 require 'codigo/diretorio/inimigo'
 require 'codigo/diretorio/bala'
+require 'codigo/diretorio/canhao'
 require 'codigo/bibliotecas/fisica'
 
 function jogo_load()
 	heroina=heroina_load()
 	castelo=castelo_load()
 	inimigos=inimigos_load()
+	canhao=canhao_load(castelo)
 	balas=bala_load()
 
 	morte=false
@@ -18,12 +20,13 @@ function jogo_update(dt)
 		inimigos=inimigos_update(dt)
 		heroina=heroina_update(dt)
 		balas=bala_update(dt,heroina)
+		balas=bala_updateCanhao(dt,canhao.direito)
+		balas=bala_updateCanhao(dt,canhao.esquerdo)
 		
 		-- detecta a colisao da heorina e dos inimigos com o castelo
 		for i=castelo.y, castelo.altura, castelo.alturaBloco do
 			for j=castelo.x, castelo.largura, castelo.larguraBloco do
-				if ((i==castelo.y or j==castelo.x or i==castelo.altura-5) and 
-					j<=castelo.meio) or ((i==175 or i==310) and j>castelo.meio) or 
+				if ((i==castelo.y or j==castelo.x or i==castelo.altura-5) and j<=castelo.meio) or ((i==175 or i==310) and j>castelo.meio) or 
 					(j==castelo.meio and (i<=175 or i>=310)) or (j==castelo.largura and (i==190 or i==295)) then
 				 	heroina=detectarColisaoFora(heroina,j,i,castelo.alturaBloco,castelo.larguraBloco)
 				 	for k,inimigo in pairs(inimigos) do
@@ -45,18 +48,11 @@ function jogo_update(dt)
 		end
 
 	 	-- mata os inimigos
-	 	for i,bala in pairs(balas) do
-	 		for j,inimigo in pairs(inimigos) do
-				if detectarColisao(bala,inimigo) then
-					balas_remove(i)
-					inimigos_remove(j)
-				end
-			end	 	
-		end	
+	 	balaAtingeInimigo(balas,inimigos)	
 
 		-- detecta colisão com o inimigo
-		for i=1, #inimigos do
-			if detectarColisao(heroina,inimigos[i]) then
+		for i,inimigo in pairs(inimigos) do
+			if detectarColisao(heroina,inimigo) then
 				morte=true
 			 	break
 			end	 	
@@ -71,6 +67,7 @@ function jogo_draw()
 	bala_draw()
 	heroina_draw()
 	inimigos_draw()
+	canhao_draw()
 
 	if morte then
 		love.graphics.clear()
